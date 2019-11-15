5db8d6d411d9bcb152f937752c64f70f4f9e6d33
Ieva Ozolite <ieva.ozolite@gmail.com>
2019-11-15 20:25:14 +0200

    add cypress and first test

---
+++ cypress/integration/startForm.spec.js
@@ -0,0 +1,21 @@
+context("Multistep form", () => {
+	beforeEach(() => {
+    cy.visit('http://localhost:8080/')
+	})
+	describe('First step', () => {
+		it('should have instruction text', () => {
+			cy.get('.form__step .step__text')
+			.should('have.text', "Start checkout process")
+		}) 
+		it('should have button with text', () => {
+			cy.get('.form__step button')
+			.should('have.text', "Start")
+		}) 
+		it('click button and start the form', () => {
+			cy.get('.form__step button')
+			.click()
+			.get(".form__step").should("be.hidden")
+
+		}) 
+	})
+})
--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,59 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <section class="form__step">
+      <p class="step__text">Start checkout process</p>
+      <button class="btn" @click="start">Start</button>
+    </section>
+    <form></form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  data: function() {
+    return {
+      users: [],
+      loading: true,
+      isError: false,
+      errorText: "No user found with this phone number"
+    };
+  },
+  methods: {
+    start() {
+      console.log("test");
+      alert("test");
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.btn {
+  background: lightslategray;
+  border-radius: 5px;
+  padding: 1em;
+  border: none;
+  cursor: pointer;
+}
+.form__step {
+  border: 1px solid magenta;
+  padding: 3em;
+  max-width: 300px;
+  margin: 0 auto;
 }
 </style>
